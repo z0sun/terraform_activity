977d7af2c87c5f271fcd553639b741cda661ed74
Joseph White <135557197+z0sun@users.noreply.github.com>
2023-10-10 16:13:39 -0400

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install -y openjdk-11-jre
+
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update
+sudo apt install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins >> ~/Status.txt
